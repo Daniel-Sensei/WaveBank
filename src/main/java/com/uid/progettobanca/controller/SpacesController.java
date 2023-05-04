58e2463bbc7d2ccd307a6e6851db33453e0735d4
peter <petergattuso21@gmail.com>
2023-05-04 17:23:27 +0200

    spaces pages and controller

---
+++ src/main/java/com/uid/progettobanca/controller/SpacesController.java
@@ -0,0 +1,41 @@
+package com.uid.progettobanca.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class SpacesController {
+
+    @FXML
+    private Text MainSaldo;
+
+    @FXML
+    private Button Stats;
+
+    @FXML
+    private HBox listOfSpace;
+
+    @FXML
+    private Button newSpace;
+
+    @FXML
+    private Pane space;
+
+    @FXML
+    private Button spaceDoor;
+
+    @FXML
+    private Text spaceName;
+
+    @FXML
+    private Text spaceSaldo;
+
+    @FXML
+    void createNewSpace(MouseEvent event) {
+
+    }
+
+}
