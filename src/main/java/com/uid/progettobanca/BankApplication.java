3011b9eadf3328a90d25a236fe6ac7d1bdb8a4c3
Daniel-Sensei <crcdnl02e28m208a@studenti.unical.it>
2023-05-04 13:39:59 +0200

    Struttura Main con MenuBar + Home

--- src/main/java/com/uid/progettobanca/BankApplication.java
+++ src/main/java/com/uid/progettobanca/BankApplication.java
@@ -2,7 +2,10 @@ package com.uid.progettobanca;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +13,17 @@ import java.io.IOException;
 public class BankApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BankApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent menuBar = FXMLLoader.load(getClass().getResource("menuBar.fxml"));
+        Parent home = FXMLLoader.load(BankApplication.class.getResource("hello-view.fxml"));
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(menuBar);
+        BorderPane.setAlignment(menuBar, Pos.CENTER_RIGHT);
+        borderPane.setCenter(home);
+
+        Scene scene = new Scene(borderPane, 800, 600);
+
+        stage.setTitle("BankApplication");
         stage.setScene(scene);
         stage.show();
     }
