59cba980bd096e8c368b9404b8122a59bd857205
peter <petergattuso21@gmail.com>
2023-05-05 18:53:58 +0200

    fixed single space generator, modified the spaces.fxml

--- src/main/java/com/uid/progettobanca/BankApplication.java
+++ src/main/java/com/uid/progettobanca/BankApplication.java
@@ -1,6 +1,6 @@
 package com.uid.progettobanca;
 
-import com.uid.progettobanca.view.SceneHandler;
+import com.uid.progettobanca.controller.SpacesController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
@@ -9,16 +9,27 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.Objects;
-
-import static com.sun.javafx.scene.control.skin.Utils.getResource;
 
 public class BankApplication extends Application {
-
     @Override
     public void start(Stage stage) throws IOException {
-        SceneHandler.getInstance().init(stage);
+        Parent menuBar = FXMLLoader.load(getClass().getResource("menuBar.fxml"));
+        Parent home = FXMLLoader.load(BankApplication.class.getResource("spaces.fxml"));
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(menuBar);
+        BorderPane.setAlignment(menuBar, Pos.CENTER_RIGHT);
+        borderPane.setCenter(home);
+
+        Scene scene = new Scene(borderPane, 1280, 720);
+
+        stage.setTitle("BankApplication");
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/uid/progettobanca/controller/SpacesController.java
+++ src/main/java/com/uid/progettobanca/controller/SpacesController.java
@@ -1,30 +1,48 @@
 package com.uid.progettobanca.controller;
 
+import com.uid.progettobanca.BankApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
+import javafx.scene.layout.FlowPane;
+
+import java.io.File;
+import java.io.IOException;
 
 public class SpacesController {
-
-    @FXML
-    private Text MainSaldo;
 
     @FXML
     private Button Stats;
 
     @FXML
-    private HBox listOfSpace;
+    private Label Title;
+
+    @FXML
+    private FlowPane listOfSpaces;
 
     @FXML
     private Button newSpace;
 
     @FXML
-    void createNewSpace(MouseEvent event) {
+    private Label saldo;
 
-    }
+    @FXML
+    void createNewSpace(MouseEvent event) throws IOException {
+        try{
+            FXMLLoader space = new FXMLLoader(BankApplication.class.getResource("/single-space.fxml"));
+            Parent scene = space.load();
+            listOfSpaces.getChildren().add(scene);
+
+        }
+        catch(IOException e){
+            System.out.println("contatto fallito");
+        }
+        }
 
 }
+
+
+
