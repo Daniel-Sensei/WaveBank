bae818701d242a10965207d869662d2694af13c4
Daniel-Sensei <crcdnl02e28m208a@studenti.unical.it>
2023-05-08 16:39:27 +0200

    Aggiunta stili chiaro e scuro

--- src/main/java/com/uid/progettobanca/BankApplication.java
+++ src/main/java/com/uid/progettobanca/BankApplication.java
@@ -4,6 +4,7 @@ import com.uid.progettobanca.controller.SpacesController;
 import com.uid.progettobanca.view.SceneHandler;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,6 +17,7 @@ import java.io.IOException;
 public class BankApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         //qui viene inizializzata la scena principale
         SceneHandler.getInstance().init(stage);
         //per passare alla pagina che volete usate --> SceneHandler.getInstance().<nomeMetodo()>;
