c311e006e039fedf07f3c77e518eb1eb12be9adc
Fortu <ciberuomo@gmail.com>
2023-05-16 12:18:50 +0200

    i grafici dovrebbero mostrare i progressi del conto nel tempo, ma credo non ci siano transazioni salvate

--- src/main/java/com/uid/progettobanca/controller/ManageController/ManageController.java
+++ src/main/java/com/uid/progettobanca/controller/ManageController/ManageController.java
@@ -1,5 +1,8 @@
 package com.uid.progettobanca.controller.ManageController;
 
+        import com.uid.progettobanca.BankApplication;
+        import com.uid.progettobanca.model.DAO.TransazioniDAO;
+        import com.uid.progettobanca.model.Transazione;
         import com.uid.progettobanca.view.SceneHandler;
         import javafx.animation.Interpolator;
         import javafx.animation.KeyFrame;
@@ -16,6 +19,11 @@ package com.uid.progettobanca.controller.ManageController;
         import javafx.scene.image.ImageView;
         import javafx.scene.layout.HBox;
         import javafx.util.Duration;
+
+        import java.sql.SQLException;
+        import java.time.LocalDateTime;
+        import java.util.ArrayList;
+        import java.util.List;
 
 public class ManageController {
     int numcarte=0;
@@ -62,15 +70,57 @@ public class ManageController {
         /*destra.setVisible(false);
         sinistra.setVisible(false);*/
 
+        //prendi tutte le transazioni dell'utente
+        try {
+            XYChart.Series data = new XYChart.Series();
+            List<Transazione> transazioni = TransazioniDAO.selectByIban(BankApplication.getCurrentlyLoggedIban());
 
+            double baseline=0;
         //prendi le transazioni degli ultimi 30 giorni in ordine cronologico
-        //per ogni transazione, prendi il giorno e l'importo
-        //dividi i 30 giorni in 10 blocchi e sommali fra loro
-        //prendi il saldo all'inizio dei 30 giorni
-        //somma volta per volta i blocchi al saldo iniziale
-        //inserisci i valori nel grafico
+            //trova il saldo iniziale
+            for(int i=0; i<transazioni.size(); i++){
+                LocalDateTime now = LocalDateTime.now();
+                LocalDateTime thirtyDaysAgo = now.minusDays(30);
+                if (transazioni.get(i).getDateTime().isBefore(thirtyDaysAgo)) {
+                    baseline += transazioni.get(i).getImporto();
+                }
+            }
 
-        XYChart.Series data = new XYChart.Series();
+            List<Double> days = new ArrayList<>();
+            for(int i =0; i<10; i++){
+                days.add(0.0);
+            }
+            //somma ogni 3 giorni le transazioni al saldo iniziale
+            for(int i=0; i<transazioni.size(); i++){
+                int iterations=1;
+                for(LocalDateTime j=LocalDateTime.now().minusDays(30); j.isEqual(LocalDateTime.now()); j.plusDays(3)){
+                    if(transazioni.get(i).getDateTime().isAfter(j) && transazioni.get(i).getDateTime().isBefore(j.plusDays(3))){
+                        days.set(iterations, days.get(iterations)+transazioni.get(i).getImporto());
+                    }
+                    iterations++;
+                }
+
+               /* LocalDateTime now = LocalDateTime.now();
+                LocalDateTime thirtyDaysAgo = now.minusDays(30);
+                if (transazioni.get(i).getDateTime().isAfter(thirtyDaysAgo)) {
+                    baseline += transazioni.get(i).getImporto();
+                    if(transazioni.get(i).getDateTime().getDayOfMonth()%3==0){
+                        // inserisci i valori nel grafico
+                        data.getData().add(new XYChart.Data(String.valueOf(transazioni.get(i).getDateTime().getDayOfMonth()), baseline));
+                    }
+                } */
+            }
+            for(int i=0; i<days.size(); i++){
+                days.set(i, days.get(i)+baseline);
+                data.getData().add(new XYChart.Data(String.valueOf(i), days.get(i)));
+            }
+
+
+    } catch (SQLException e) {
+        throw new RuntimeException(e);  //dobbiamo vedere come evitare il try catch
+    }
+
+      /*  XYChart.Series data = new XYChart.Series();
 
         data.getData().add(new XYChart.Data("1", 23));
         data.getData().add(new XYChart.Data("4", 14));
@@ -82,12 +132,7 @@ public class ManageController {
         data.getData().add(new XYChart.Data("22", 45));
         data.getData().add(new XYChart.Data("25", 43));
         data.getData().add(new XYChart.Data("28", 17));
-        data.getData().add(new XYChart.Data("31", 29));
-
-        chart.setHorizontalGridLinesVisible(false);
-        chart.setVerticalGridLinesVisible(false);
-
-        chart.getData().add(data);
+        data.getData().add(new XYChart.Data("31", 29));*/
 
 
     }
