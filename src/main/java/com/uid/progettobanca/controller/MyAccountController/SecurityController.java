1d3e78a4bd684aff36c46cbc96dceb515551e0f8
Fortu <ciberuomo@gmail.com>
2023-05-21 15:29:25 +0200

    pagina cambio password funzionante

--- src/main/java/com/uid/progettobanca/controller/MyAccountController/SecurityController.java
+++ src/main/java/com/uid/progettobanca/controller/MyAccountController/SecurityController.java
@@ -1,4 +1,64 @@
 package com.uid.progettobanca.controller.MyAccountController;
 
+import com.uid.progettobanca.BankApplication;
+import com.uid.progettobanca.model.DAO.UtentiDAO;
+import com.uid.progettobanca.view.SceneHandler;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.*;
+import java.net.URI;
+import java.sql.SQLException;
+
+
 public class SecurityController {
+
+    @FXML
+    private PasswordField confirmPsw;
+
+    @FXML
+    private PasswordField newPsw;
+
+    @FXML
+    private PasswordField oldPsw;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    void pswGuide(MouseEvent event) {
+        try {
+            Desktop.getDesktop().browse(new URI("https://www.youtube.com/watch?v=z1VBlur77cI&ab_channel=ぽにきゃん-AnimePONYCANYON"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    @FXML
+    void sendButtonPressed (ActionEvent event) {
+        try {
+            int user = UtentiDAO.login(emailField.getText(), oldPsw.getText());
+            if(user!=0){
+                if(newPsw.getText().equals(confirmPsw.getText())){
+                    UtentiDAO.passwordChange(emailField.getText(), newPsw.getText());
+                    SceneHandler.getInstance().showInfo("Cambio password", "Cambio password effettuato", "Hai cambiato password!");
+                    SceneHandler.getInstance().setPage(SceneHandler.MY_ACCOUNT_PATH + "myAccount.fxml");
+                }
+                else {
+                    SceneHandler.getInstance().showError("Errore", "Password errata", "Le due password non coincidono");
+                }
+            }
+            else {
+                System.out.println("nuova psww: " + oldPsw.getText());
+                System.out.println("vecchia psww: " + UtentiDAO.selectByUserId(BankApplication.getCurrentlyLoggedUser()).getPassword());
+                SceneHandler.getInstance().showError("Errore", "Password errata", "Le vecchia password è errata");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
