efd4d033096279525283ece75e77297d8ce1469f
peter <petergattuso21@gmail.com>
2023-06-07 15:15:40 +0200

    traduzioni

--- src/main/java/com/uid/progettobanca/controller/SpacesController/SpaceTransactionComboBoxController.java
+++ src/main/java/com/uid/progettobanca/controller/SpacesController/SpaceTransactionComboBoxController.java
@@ -4,13 +4,11 @@ import com.uid.progettobanca.controller.GenericController;
 import com.uid.progettobanca.model.SpaceTransactionManager;
 import com.uid.progettobanca.model.SpacesManager;
 import com.uid.progettobanca.view.FormUtils;
-import com.uid.progettobanca.view.ImageUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.sql.SQLException;
