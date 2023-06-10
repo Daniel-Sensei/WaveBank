8a8190e534a5d356ba07111a278a95183095d187
gianpoo <campanella2001@gmail.com>
2023-06-10 13:18:39 +0200

    added comments to BankApplication file

--- src/main/java/com/uid/progettobanca/BankApplication.java
+++ src/main/java/com/uid/progettobanca/BankApplication.java
@@ -6,43 +6,24 @@ import com.uid.progettobanca.view.SceneHandler;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-
 public class BankApplication extends Application {
-
-    //ho inserito qui i currently logged user per poterli usare in tutto il programma
-    //spostateli se non vi piace la posizione
-
-    private static int currentlyLoggedUser = 0;
-    public static void setCurrentlyLoggedUser(int user) {currentlyLoggedUser = user;}
-    public static int getCurrentlyLoggedUser() {return currentlyLoggedUser;}
-
-    private static String currentlyLoggedIban = "IT6557741166469259890884056";
-    public static void setCurrentlyLoggedIban(String iban) {currentlyLoggedIban = iban;}
-    public static String getCurrentlyLoggedIban() {return currentlyLoggedIban;}
-
-    private static int currentlyLoggedMainSpace = 0;
-    public static void setCurrentlyLoggedMainSpace(int space) {currentlyLoggedMainSpace = space;}
-    public static int getCurrentlyLoggedMainSpace() {return currentlyLoggedMainSpace;}
-
-    private static String currentlyLoggedMail = "";
-    public static void setCurrentlyLoggedMail(String mail) {currentlyLoggedMail = mail;}
-    public static String getCurrentlyLoggedMail() {return currentlyLoggedMail;}
-
 
     @Override
     public void start(Stage stage) {
 
-        DBService dbs = new DBService();
-        dbs.restart();
+        DBService dbs = new DBService(); // creating a new DBService object to initialize the database (if it's not already initialized)
+        dbs.restart(); // starting the DBService
 
         dbs.setOnSucceeded(e -> {
-            //qui viene caricato il file di settings
+            // load settings from file (or create a new one if it doesn't exist)
             Settings.loadSettings();
-            //qui viene inizializzata la scena principale con menù bar ed home di default
+            // create the login scene and effectively starts the application for the user
             SceneHandler.getInstance().createLoginScene(stage);
         });
 
         dbs.setOnFailed(e -> {
+            // if the database initialization fails, the application is closed
+            // this shouldn't happen unless the application doesn't have the permission to write in the database file or the database file is corrupted, etc.
             throw new RuntimeException(e.getSource().getException());
         });
 
@@ -51,4 +32,77 @@ public class BankApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
+    /**********************************************************************************************/
+
+    // these are the variables that are used to keep track of the currently logged user information
+
+    private static int currentlyLoggedUser = 0; // id of the user
+
+    /**
+     * Set the currently logged user id
+     *
+     * @param user an int representing the id of the user
+     */
+    public static void setCurrentlyLoggedUser(int user) {currentlyLoggedUser = user;}
+
+    /**
+     * Get the currently logged user id
+     *
+     * @return an int representing the id of the user
+     */
+    public static int getCurrentlyLoggedUser() {return currentlyLoggedUser;}
+
+
+    private static String currentlyLoggedIban = ""; // iban of the user
+
+    /**
+     * Set the currently logged user IBAN
+     *
+     * @param iban a String representing the IBAN of the user
+     */
+    public static void setCurrentlyLoggedIban(String iban) {currentlyLoggedIban = iban;}
+
+    /**
+     * Get the currently logged user IBAN
+     *
+     * @return a String representing the IBAN of the user
+     */
+    public static String getCurrentlyLoggedIban() {return currentlyLoggedIban;}
+
+
+    private static int currentlyLoggedMainSpace = 0; // main space of the user
+
+    /**
+     * Set the currently logged user main space id
+     *
+     * @param space an int representing main space's id of the user
+     */
+
+    public static void setCurrentlyLoggedMainSpace(int space) {currentlyLoggedMainSpace = space;}
+
+    /**
+     * Get the currently logged user main space id
+     *
+     * @return an int representing main space's id of the user
+     */
+    public static int getCurrentlyLoggedMainSpace() {return currentlyLoggedMainSpace;}
+
+
+    private static String currentlyLoggedMail = ""; // mail of the user
+
+    /**
+     * Set the currently logged user mail
+     *
+     * @param mail a String representing the mail of the user
+     */
+    public static void setCurrentlyLoggedMail(String mail) {currentlyLoggedMail = mail;}
+
+    /**
+     * Get the currently logged user mail
+     *
+     * @return a String representing the mail of the user
+     */
+    public static String getCurrentlyLoggedMail() {return currentlyLoggedMail;}
 }
