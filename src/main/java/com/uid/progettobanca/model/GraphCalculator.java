239a1c5328b68cb3a8fb239a6f906a8ccb2f0315
Fortu <ciberuomo@gmail.com>
2023-06-14 22:09:45 +0200

    FIXATO GRAFICO MI SPARO

--- src/main/java/com/uid/progettobanca/model/GraphCalculator.java
+++ src/main/java/com/uid/progettobanca/model/GraphCalculator.java
@@ -38,9 +38,9 @@ public class GraphCalculator {
         LocalDateTime firstDay = LocalDateTime.now().minusDays(DaysInterval);
         int arrPos=0;
         for(int i=0; i<transazioni.size(); i++) {
-            if(((!withTag || transazioni.get(i).getTag().equals(tag)) && transazioni.get(i).getDateTime().isAfter(firstDay)))
+            if(((!withTag || transazioni.get(i).getTag().equals(tag)) && transazioni.get(i).getDateTime().isAfter(firstDay))){
                 arrPos = Math.toIntExact(ChronoUnit.DAYS.between(firstDay, transazioni.get(i).getDateTime()));
-                DaysValues.set(arrPos, transazioni.get(i).getImporto()+DaysValues.get(arrPos));
+                DaysValues.set(arrPos, transazioni.get(i).getImporto()+DaysValues.get(arrPos));}
         }
         return DaysValues;
     }
